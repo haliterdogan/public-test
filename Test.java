941e19d224fb49e06fdda905ee3a4c80a0728dd6
Halit Erdogan <halit@halit-Inspiron-560>
2012-09-05 00:04:02 -0500

    Comments added.

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
+/**
+ * Testing class.
+ */
 public class Test {
 
+    /**
+     * Main test application.
+     * 
+     * @param args None.
+     */
     public static void main(String[] args) {
     
 	System.out.println("Halit");
